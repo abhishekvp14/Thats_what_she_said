671db8e75698f4350a56ff972abdfb63f19c8164
Abhishek VP <as826119@gmail.com>
2021-11-15 19:37:34 +0530

    fixed responses

--- listen.py
+++ listen.py
@@ -12,7 +12,7 @@ chunk_size = 2048
 r = sr.Recognizer()
 
 text = ''
-while text.lower() != 'stop':
+while text.lower() != 'stop listening':
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         print("Say something: ")
@@ -21,13 +21,16 @@ while text.lower() != 'stop':
         try:
             text = r.recognize_google(audio)
             #print("You said: " + text)
+            if(search(text)):
+                michael()
+            elif text == 'stop listening':
+                exit(0)
+            else:
+                okay()
             
         except sr.UnknownValueError:
             print("Didnt get you.")
         except sr.RequestError as e:
             print("Could not request results from Google SRS; {0}".format(e))
             
-    if(search(text)):
-        michael()
-    else:
-        okay()
+    
